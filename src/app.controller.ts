603140e094d9aa2d9e48a154c7cb8325d5a52970
Henrico Aldy Ferdian <henricoaldyferdian@hotmail.com>
2021-05-13 21:28:33 +0800

    Login auth JWT completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  // This route does not need to authenticate users
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  // This route need users to be authenticated
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
